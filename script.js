1a5cfd475a660ecbcf707ead7d296314bfc4fe2e
sahumo <7523460@gmail.com>
2021-08-19 20:26:21 +0300

    operator  break

--- script.js
+++ script.js
@@ -15,9 +15,11 @@ let num = 50;
 // while (num < 55);
 
 
-for (let i = 1; i < 0; i++) {
-console.log(num);
-num++;
+for (let i = 1; i < 10; i++) {
+if ( i === 6) {
+    break;
+}
+console.log(i);
 }
 
 
